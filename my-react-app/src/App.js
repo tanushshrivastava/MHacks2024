37cb2ac09d24b42c90847a61513ed54827a3db47
tanush <tanush.shrivastava@gmail.com>
2024-09-23 16:39:31 -0500

    netlify instances

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,27 +1,28 @@
 import React, { useState } from 'react';
 import './App.css';
-
+const url2 = 'http://localhost:8000'
+const url1 = 'https://mhacks2024.onrender.com'
 function App() {
   const [message, setMessage] = useState('');
   const [time, setTime] = useState('');
   const [featureEnabled, setFeatureEnabled] = useState(false);
 
   const fetchMessage = () => {
-    fetch('http://127.0.0.1:8000/api/message')
+    fetch(url1 + '/api/message')
       .then(response => response.json())
       .then(data => setMessage(data.message))
       .catch(err => console.error('Error fetching data:', err));
   };
 
   const fetchTime = () => {
-    fetch('http://127.0.0.1:8000/api/time')
+    fetch(url1 + '/api/time')
       .then(response => response.json())
       .then(data => setTime(data.time))
       .catch(err => console.error('Error fetching data:', err));
   };
 
   const toggleFeature = () => {
-    fetch('http://127.0.0.1:8000/api/toggle')
+    fetch(url1 + '/api/toggle')
       .then(response => response.json())
       .then(data => setFeatureEnabled(data.featureEnabled))
       .catch(err => console.error('Error fetching data:', err));
